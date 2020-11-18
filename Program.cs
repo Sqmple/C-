b309272460038461babcd003b7d7703f45e5659b
Sqmple <az63.zov@gmail.com>
2020-11-18 23:32:26 +0200

    second

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b;
+            char c;
+            Console.WriteLine("Введите первое число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выберите операцию +,-,*,/,%: ");
+            c = Convert.ToChar(Console.ReadLine());
+            if(c == '+'){
+                double result = a + b;
+                Console.WriteLine("Ответ: "+result);
+            }
+            else if(c == '-'){
+                double result = a - b;
+                Console.WriteLine("Ответ: "+result);
+            }
+            else if(c == '*'){
+                double result = a * b;
+                Console.WriteLine("Ответ: "+result);
+            }
+            else if(c == '/'){
+                double result = a / b;
+                Console.WriteLine("Ответ: "+result);
+            }
+            else if(c == '%'){
+                double result = a % b;
+                Console.WriteLine("Ответ: "+result);
+            }
+            else{
+                Console.WriteLine("Ошибка");
+            }
+        }
+    }
+}
